91677132fc81817ea33d51b98559b5a80da3bd44
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-23 14:35:33 +0200

    filtered msg from json fetch and slug

--- src/pages/GetMsg.js
+++ src/pages/GetMsg.js
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const GetMsg = () => {
   const [test, setTest] = useState([]);
+  const [association, setAssociation] = useState([]);
 
   useEffect(() => {
+    fetchDataParams();
     fetchdata();
   }, []);
+
+  const { slug } = useParams();
+
+  const fetchDataParams = async () => {
+    const request = await fetch(`http://localhost:5000/associations/${slug}`);
+    const response = await request.json();
+    setAssociation(response);
+    const filtered = test.filter((konexio) => {
+      return konexio.slug === slug;
+    });
+    console.log(filtered);
+  };
 
   const fetchdata = async () => {
     const request = await fetch(
@@ -13,20 +28,11 @@ const GetMsg = () => {
     );
     const response = await request.json();
     setTest(response);
+
+    console.log(test);
   };
 
-  const filteredArray = () => {
-    const filtered = test.filter((konexio) => {
-      return konexio.name;
-    });
-    console.log(filtered);
-  };
-  console.log(test);
-  return (
-    <div>
-      <button onClick={filteredArray}>ttest</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default GetMsg;
