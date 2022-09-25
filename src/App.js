fed43c2e083bcbaeea47812a8554187dfcb061d3
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-25 16:07:14 +0200

    restart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./assets/styles/index.css";
-import Association from "./pages/Association";
-import Home from "./pages/Home";
-import GetMsg from "./pages/GetMsg";
-
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/associations/:slug" element={<Association />} />
-          <Route path="/associations/:slug/msg" element={<GetMsg />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <>dfbdbq</>;
 };
 
 export default App;
