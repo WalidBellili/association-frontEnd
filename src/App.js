ea648431bab52ed9d6c5130a5ff0038cd766189a
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-25 16:29:46 +0200

    router done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./pages/Admin";
+import Association from "./pages/Association";
 import Home from "./pages/Home";
+import NewMessage from "./pages/NewMessage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -7,6 +11,10 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/:association_slug" element={<Association />} />
+          <Route path="/new-message" element={<NewMessage />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Association.js
+++ src/pages/Association.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Association = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div>asso</div>;
 };
 
 export default Association;
