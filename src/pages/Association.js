06e528fb1a376eca2198458f7ac567662b11825c
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-25 16:21:10 +0200

    test netlify env

---
+++ src/pages/Association.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Association = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Association;
---
+++ src/pages/NewMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const newMessage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default newMessage;
