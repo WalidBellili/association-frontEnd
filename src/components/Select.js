71ace6c579c5fc9e53b589620e6dc68646ca076c
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-26 14:43:54 +0200

    component select with a map direct from the api whith options and values and a map to render

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Select = () => {
-  return <div></div>;
+const Select = ({ onChange, options, value }) => {
+  return (
+    <select onChange={onChange} value={value}>
+      <option value="">Choose an Association</option>
+      {options.map((option) => {
+        return <option value={option.value}>{option.text}</option>;
+      })}
+    </select>
+  );
 };
 
 export default Select;
--- src/pages/NewMessage.js
+++ src/pages/NewMessage.js
@@ -1,14 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import H2 from "../components/H2";
 import Input from "../components/Input";
+import Select from "../components/Select";
 
 const NewMessage = () => {
   const [message, setMessage] = useState("");
 
+  const [options, setOptions] = useState([]);
+  const [associationSlug, setAssociationSlug] = useState("");
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    selectOption();
+  }, []);
   const handleMessage = (e) => {
     setMessage(e.target.value);
   };
-  console.log(message);
+  const handleAssociationSlug = (e) => {
+    setAssociationSlug(e.target.value);
+  };
+
+  const selectOption = async () => {
+    const request = await fetch(
+      `${process.env.REACT_APP_API_URL}${"associations"}`
+    );
+    const response = await request.json();
+
+    const mapForOptions = response.map((association) => {
+      return {
+        value: association.slug,
+        text: association.name,
+      };
+    });
+    setOptions(mapForOptions);
+  };
+  // const options = [
+  //   {
+  //     value: "",
+  //     text: "Select an option",
+  //   },
+  //   {
+  //     value: "les-restos-du-coeurs",
+  //     text: "Les restos du coeur",
+  //   },
+  //   {
+  //     value: "unicef",
+  //     text: "Unicef",
+  //   },
+  //   {
+  //     value: "konexio",
+  //     text: "Konexio",
+  //   },
+  // ];
 
   return (
     <>
@@ -20,6 +63,11 @@ const NewMessage = () => {
           type="text"
           label="Object"
           placeholder="Object"
+        />
+        <Select
+          onChange={handleAssociationSlug}
+          options={options}
+          value={associationSlug}
         />
       </form>
     </>
